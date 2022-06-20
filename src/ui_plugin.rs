8eeeeea60ca85c1cc44e32c2a0c2ed0393cc3d56
follower <follower@rancidbacon.com>
2022-06-21 05:06:47 +1200

    Formatting.

--- src/ui_plugin.rs
+++ src/ui_plugin.rs
@@ -4,8 +4,12 @@ pub(crate) struct UiPlugin;
 
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut App) {
+        //
+
         info!("Building `{}`...", "UiPlugin");
 
         app.add_plugin(crate::ui_inspector::UiInspectorPlugin);
+
+        //
     }
 }
