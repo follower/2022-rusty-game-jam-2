aff0488f38b2e1caf3422f7f58202d1e26f9e606
follower <follower@rancidbacon.com>
2022-06-20 22:19:18 +1200

    Untabify...
    ...and update `.emacs` on this machine finally. :D

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,13 @@ const APP_TITLE: &str = "Quacks Like a Dog (Rusty Jam #2)";
 pub fn start_app() {
 
     App::new()
-	.insert_resource(WindowDescriptor {
-	    title: APP_TITLE.into(),
-	    ..default()
-	})
-	.add_plugins(DefaultPlugins)
-	.add_plugin(ui_plugin::UiPlugin)
-	.add_startup_system(log_application_start_system)
-	.run();
+        .insert_resource(WindowDescriptor {
+            title: APP_TITLE.into(),
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(ui_plugin::UiPlugin)
+        .add_startup_system(log_application_start_system)
+        .run();
 
 }
