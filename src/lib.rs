52e3a857a1079837079e095b823af8c7e03fdd0a
follower <follower@rancidbacon.com>
2022-06-20 21:47:40 +1200

    Add Bevy App/Window creation boilerplate.
    "Quacks Like a Dog" is officially underway...

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 
+use bevy::prelude::*;
+
+const APP_TITLE: &str = "Quacks Like a Dog (Rusty Jam #2)";
+
 pub fn start_app() {
 
     println!("Hello, world!");
+
+    App::new()
+	.insert_resource(WindowDescriptor {
+	    title: APP_TITLE.into(),
+	    ..default()
+	})
+	.add_plugins(DefaultPlugins)
+	.run();
 
 }
