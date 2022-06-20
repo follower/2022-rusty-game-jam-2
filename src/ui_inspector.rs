3d31223132d287388ac86bd05e57ebc318884017
follower <follower@rancidbacon.com>
2022-06-21 05:09:34 +1200

    Formatting.

--- src/ui_inspector.rs
+++ src/ui_inspector.rs
@@ -9,12 +9,16 @@ struct UiInspectorState {
 
 impl Plugin for UiInspectorPlugin {
     fn build(&self, app: &mut App) {
+        //
+
         info!("Building `{}`...", "UiInspectorPlugin");
 
         app.init_resource::<UiInspectorState>()
             .init_resource::<bevy_inspector_egui::WorldInspectorParams>()
             .add_plugin(bevy_inspector_egui::WorldInspectorPlugin::new())
             .add_startup_system(configure_from_environment_startup_system);
+
+        //
     }
 }
 
@@ -22,8 +26,12 @@ fn configure_from_environment_startup_system(
     mut ui_state: ResMut<UiInspectorState>,
     mut inspector_state: ResMut<bevy_inspector_egui::WorldInspectorParams>,
 ) {
+    //
+
     info!("System setup...");
 
     ui_state.visible = !std::env::var_os("QLAD_INSPECTOR").is_none();
     inspector_state.enabled = ui_state.visible;
+
+    //
 }
